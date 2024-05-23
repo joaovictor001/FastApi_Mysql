5f8c3d3bd38901cda9206f6638c5846d342a5513
joaovictor001 <joaocaralhoborges@outlook.com>
2024-05-23 09:03:52 -0300

    Delete method

--- api/v1/endpoints/curso.py
+++ api/v1/endpoints/curso.py
@@ -78,6 +78,19 @@ async def put_curo(curso_id: int, curso: CursoSchemam, db: AsyncSession= Depends
         else: 
             raise HTTPException(detail='Curso não encotrado ', status_code=status.HTTP_404_NOT_FOUND)
         
+@router.delete('/{curso_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_curso(curso_id:int,db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        result = await session.execute(query)
+        curso_del = result.scalar_one_or_none()
+        if curso_del:
+            await session.delete(curso_del)
+            await session.commit()
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+        else:
+            raise(HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso dnão encontrado"))
+        
         
 
 
